9273616ab6849eb5c11ba7448509ba89ea3ed507
Silvano Stralla <silvano.stralla@sistrall.it>
2016-10-25 14:38:12 +0000

    Site updated to e24640d

--- assets/javascripts/all.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){var t="http://api.facebook.com/restserver.php?method=links.getStats&urls="+$("meta[property='og:url']").attr("content");$.getJSON(t,{format:"json"}).done(function(t){$("[js-meta--like-count]").each(function(){$(this).html(t[0].like_count)})})});
